0c6adfea7fdb0bf1d0bbd3341e2e22e3ed197c59
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2024-12-01 11:00:08 -0500

    modificacion a list.pl

--- cgi-bin/list.pl
+++ cgi-bin/list.pl
@@ -4,36 +4,28 @@ use warnings;
 use CGI;
 use CGI::Carp 'fatalsToBrowser';
 
-# Crear objeto CGI
 my $q = CGI->new();
 print $q->header('text/html; charset=UTF-8');
 print "<html lang=\"es\"><head><title>Listado de Páginas</title></head><body>";
 
-# Ruta al archivo de datos
 my $data_file = "/var/www/html/pages/pages_data.txt";
 
-# Verificar si el archivo de datos existe
 if (-e $data_file) {
     open my $fh, '<', $data_file or die "No se puede abrir el archivo de datos: $!";
     while (my $line = <$fh>) {
         chomp $line;
         my ($titulo, $contenido, $ruta) = split /\|/, $line;
 
-        # Mostrar el nombre de la página como un hiperenlace
-        print "<p><a href='$ruta'>$titulo</a> ";
-
-        # Botón de eliminación (X) al lado del nombre de la página
+        # Enlace al script view.pl con el nombre de la página como parámetro
+        print "<a href='/cgi-bin/view.pl?titulo=$titulo'>$titulo</a> ";
+        
+        # Botones de eliminar y editar
         print "<a href='/cgi-bin/delete.pl?titulo=$titulo' style='color:red; text-decoration:none;'>[X]</a> ";
-
-        # Botón de edición (E) - Redirige a edit.pl con el nombre de la página
-        print "<a href='/cgi-bin/edit.pl?titulo=$titulo' style='margin-left: 10px;'>[E]</a></p>";
+        print "<a href='/cgi-bin/edit.pl?titulo=$titulo' style='color:blue; text-decoration:none;'>[E]</a><br><br>";
     }
     close $fh;
 } else {
     print "<h1>No hay páginas creadas.</h1>";
 }
-
-# Enlace para regresar a new.html
 print "<br><a href='/new.html'>Regresar a crear nueva página</a>";
-
 print "</body></html>";
