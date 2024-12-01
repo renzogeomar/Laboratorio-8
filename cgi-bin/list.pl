25e3f5f0236cdf58d9ab7cf23a8eeaf4246b5ccf
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2024-12-01 01:49:16 -0500

    se agrego la funcionalidad de eliminar

--- cgi-bin/list.pl
+++ cgi-bin/list.pl
@@ -1,61 +1,39 @@
 #!/usr/bin/perl -w
 use strict;
+#!/usr/bin/perl -w
+use strict;
 use warnings;
 use CGI;
-use utf8;
 use CGI::Carp 'fatalsToBrowser';
-use File::Basename;
 
-# Crear un objeto CGI
+# Crear objeto CGI
 my $q = CGI->new();
-
-# Imprimir encabezados HTTP
 print $q->header('text/html; charset=UTF-8');
-print "<!DOCTYPE html>";
-print "<html lang=\"es\">";
-print "<head>";
-print "<title>Listado de Páginas</title>";
-print "<meta charset=\"UTF-8\">";
-print "</head>";
-print "<body>";
-print "<h1>Listado de Páginas</h1>";
+print "<html lang=\"es\"><head><title>Listado de Páginas</title></head><body>";
 
-# Incluir un estilo básico para la tabla
-print "<style>";
-print "table {width: 100%; border-collapse: collapse;} ";
-print "th, td {border: 1px solid #ddd; padding: 8px; text-align: left;} ";
-print "th {background-color: #f2f2f2;} ";
-print "</style>";
+# Ruta al archivo de datos
+my $data_file = "/var/www/html/pages/pages_data.txt";
 
-print "<table>";
-print "<thead>";
-print "<tr>";
-print "<th>Nombre de la Página</th>";
-print "<th>Acciones</th>";
-print "</tr>";
-print "</thead>";
-print "<tbody>";
+# Verificar si el archivo de datos existe
+if (-e $data_file) {
+    open my $fh, '<', $data_file or die "No se puede abrir el archivo de datos: $!";
+    while (my $line = <$fh>) {
+        chomp $line;
+        my ($titulo, $contenido, $ruta) = split /\|/, $line;
 
-# Leer archivos en la carpeta pages/
-my $directory = "/var/www/html/pages/";
-opendir(my $dir, $directory) or die "No se pudo abrir el directorio: $!";
-my @files = grep { /\.html$/ && -f "$directory/$_" } readdir($dir);
-closedir($dir);
+        # Mostrar la información de la página y agregar los botones de eliminar y "E"
+        print "<h2>$titulo</h2>";
+        print "<p>$contenido</p>";
 
-# Listar los archivos como hiperenlaces
-foreach my $file (@files) {
-    my $name = basename($file, ".html"); # Extraer el nombre base sin extensión
-    print "<tr>";
-    print "<td><a href='$directory/$file'>$name</a></td>"; # Enlace al archivo .html
-    print "<td class='actions'>";
-    print "<a href='edit.pl?file=$file' title='Editar'>E</a> ";  # Enlace para editar
-    print "<a href='delete.pl?file=$file' title='Eliminar'>X</a>"; # Enlace para eliminar
-    print "</td>";
-    print "</tr>";
+        # Botón de eliminación (X)
+        print "<a href='/cgi-bin/delete.pl?titulo=$titulo' style='color:red; text-decoration:none;'>[X]</a> ";
+
+        # Botón de edición (E) - Sin funcionalidad por ahora
+        print "<button disabled style='margin-left: 10px;'>[E]</button><br><br>";
+    }
+    close $fh;
+} else {
+    print "<h1>No hay páginas creadas.</h1>";
 }
-
-print "</tbody>";
-print "</table>";
-print "<a href='../new.html'>Crear Nueva Página</a>";
-print "</body>";
-print "</html>";
+print "<br><a href='/new.html'>Regresar a crear nueva página</a>";
+print "</body></html>";
