d8e32fd52d5f57bc6516ad9eea76e16ac30babcd
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2024-12-01 01:27:04 -0500

    correccion de errores en new.pl

--- cgi-bin/new.pl
+++ cgi-bin/new.pl
@@ -4,6 +4,7 @@ use warnings;
 use CGI;
 use utf8;
 use CGI::Carp 'fatalsToBrowser';
+use File::Basename;
 
 # Crear un objeto CGI
 my $q = CGI->new();
@@ -16,26 +17,42 @@ print "<head><title>Crear Nueva Página</title></head>";
 print "<body>";
 
 # Obtener datos del formulario
-my $titulo = $q->param('titulo');     # Título de la página
+my $titulo = $q->param('titulo');       # Título de la página
 my $contenido = $q->param('contenido'); # Contenido de la página
 
-# Validar que se recibieron datos
+# Verificación de datos recibidos
+print "<h2>Depuración: Título recibido: $titulo</h2>";
+print "<h2>Depuración: Contenido recibido: $contenido</h2>";
+
+# Validar que se recibieron todos los datos
 if ($titulo && $contenido) {
     # Crear un nombre de archivo seguro
     my $filename = $titulo;
     $filename =~ s/[^a-zA-Z0-9_-]/_/g; # Reemplazar caracteres no válidos con "_"
     $filename .= ".html";
     
-    # Ruta donde se guardará el archivo
+    # Ruta donde se guardará el archivo HTML
     my $file_path = "/var/www/html/pages/$filename";
     
-    # Crear el archivo
+    # Crear el archivo HTML con el contenido
+    print "<h3>Depuración: Intentando crear archivo en: $file_path</h3>";
     open(my $fh, '>', $file_path) or die "No se pudo crear el archivo: $!";
+
     print $fh "<!DOCTYPE html>\n<html lang=\"es\">\n<head>\n<title>$titulo</title>\n</head>\n<body>\n";
     print $fh "<h1>$titulo</h1>\n";
-    print $fh "<div>$contenido</div>\n";
+    print $fh "<div>$contenido</div>\n";  # Aquí se guarda el contenido como parte del cuerpo de la página
     print $fh "</body>\n</html>";
     close($fh);
+    
+    # Verificación de la creación del archivo
+    print "<h3>Depuración: Página HTML creada correctamente.</h3>";
+
+    # Guardar los datos de la página en un archivo de texto
+    my $data_file = '/var/www/html/pages/pages_data.txt';
+    print "<h3>Depuración: Intentando guardar datos en $data_file</h3>";
+    open(my $data_fh, '>>', $data_file) or die "No se pudo abrir el archivo de datos: $!";
+    print $data_fh "$titulo|$contenido|$file_path\n";  # Guardamos título, contenido y ruta
+    close($data_fh);
 
     # Confirmación al usuario
     print "<h1>Página creada correctamente</h1>";
